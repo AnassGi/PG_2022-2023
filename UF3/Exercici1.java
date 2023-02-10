bfef786a4280974a5dc55d1520f6394e08024cc9
AnassGI <95426424+AnassGi@users.noreply.github.com>
2023-02-10 19:44:21 +0100

    Update Exercici1.java

--- UF3/Exercici1.java
+++ UF3/Exercici1.java
@@ -1,1 +1,17 @@
-hola a tothom
+import java.io.*;
+
+public class ReadFile {
+  public static void main(String[] args) {
+    try {
+      File file = new File("files/document.txt");
+      BufferedReader reader = new BufferedReader(new FileReader(file));
+      String line;
+      while ((line = reader.readLine()) != null) {
+        System.out.println(line);
+      }
+      reader.close();
+    } catch (Exception e) {
+      System.out.println("Error al llegir el fitxer");
+    }
+  }
+}
